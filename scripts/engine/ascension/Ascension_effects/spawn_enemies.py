7505b8416a7288bef56a9332604fe3a826c12e0f
RasmusLC1 <rasmus.lc3@gmail.com>
2025-07-09 16:11:39 +0200

    Ascension work

---
+++ scripts/engine/ascension/Ascension_effects/spawn_enemies.py
@@ -0,0 +1,8 @@
+class Spawn_Enemies():
+    def __init__(self, game):
+        self.game = game
+
+    def Adjust_Difficulty(self, ascension_level):
+        pass
+
+    def Spawn_Enemy(self):
--- scripts/engine/ascension/ascension.py
+++ scripts/engine/ascension/ascension.py
@@ -1,31 +1,71 @@
 from scripts.engine.keys.keys import keys
+from scripts.engine.ascension.Ascension_effects.spawn_enemies import Spawn_Enemies
 import random
 
-NOTHING = 0
-SPAWN_ENEMY = 1
-SPAWN_TRAP = 2
-PLAYER_DEBUFF = 3
-BUFF_ENEMIES = 4
-BLOCK_DOORS = 5
-REPLACE_CHESTS = 6
-REDUCE_LIGHT = 7
-SPAWN_ELITE = 8
-INCREASE_ASCENSION = 9
+
 
 
 class Ascension():
+    NOTHING = 0
+    SPAWN_ENEMY = 1
+    SPAWN_TRAP = 2
+    PLAYER_DEBUFF = 3
+    BUFF_ENEMIES = 4
+    BLOCK_DOORS = 5
+    REPLACE_CHESTS = 6
+    REDUCE_LIGHT = 7
+    SPAWN_ELITE = 8
+    INCREASE_ASCENSION = 9
+
+
     def __init__(self, game):
         self.game = game
         self.ascension_dic = {}
+        self.spawn_enemies = Spawn_Enemies(game)
+        self.Set_Ascension_Level(0)
+
+    def Set_Ascension_Level(self, new_ascension_level):
+        self.acension_level = max(0, min(5, new_ascension_level))
+        self.Adjust_Difficulty()
+
+
+    def Adjust_Difficulty(self):
+        
+        self.spawn_enemies.Adjust_Difficulty(self.acension_level)
+
+        if self.acension_level == 0:
+            self.Set_Ascension_0_dict()
+        elif self.acension_level == 1:
+            self.Set_Ascension_1_dict()
+        elif self.acension_level == 2:
+            self.Set_Ascension_2_dict()
+        elif self.acension_level == 3:
+            self.Set_Ascension_3_dict()
+        elif self.acension_level == 4:
+            self.Set_Ascension_4_dict()
+        elif self.acension_level == 5:
+            self.Set_Ascension_5_dict()
+        else:
+            print("ERROR ASCENSION LEVEL NOT FOUND", self.acension_level)
+
+    def Trigger_Ascension_Dic(self):
+        effects = random.choices(
+                    population=list(self.ascension_dic.keys()),
+                    weights=list(self.ascension_dic.values()),
+                    k=1
+                )[0]
+        
+
+
     
-    def Ascension_0_dict(self):
+    def Set_Ascension_0_dict(self):
         self.ascension_dic = {
             NOTHING : 4,
             SPAWN_ENEMY : 0.5,
             INCREASE_ASCENSION : 0.5,
         }
 
-    def Ascension_1_dict(self):
+    def Set_Ascension_1_dict(self):
         self.ascension_dic = {
             NOTHING : 5,
             SPAWN_ENEMY : 0.3,
@@ -34,7 +74,7 @@ class Ascension():
         }
 
     
-    def Ascension_2_dict(self):
+    def Set_Ascension_2_dict(self):
         self.ascension_dic = {
             NOTHING : 6,
             SPAWN_ENEMY : 0.6,
@@ -44,7 +84,7 @@ class Ascension():
             INCREASE_ASCENSION : 0.08,
         }
 
-    def Ascension_3_dict(self):
+    def Set_Ascension_3_dict(self):
         self.ascension_dic = {
             NOTHING : 7,
             SPAWN_ENEMY : 0.7,
@@ -56,7 +96,7 @@ class Ascension():
             INCREASE_ASCENSION : 0.05,
         }
 
-    def Ascension_4_dict(self):
+    def Set_Ascension_4_dict(self):
         self.ascension_dic = {
             NOTHING : 8,
             SPAWN_ENEMY : 1,
@@ -69,7 +109,7 @@ class Ascension():
             INCREASE_ASCENSION : 0.03,
         }
 
-    def Ascension_5_dict(self):
+    def Set_Ascension_5_dict(self):
         self.ascension_dic = {
             NOTHING : 9,
             SPAWN_ENEMY : 1.1,
