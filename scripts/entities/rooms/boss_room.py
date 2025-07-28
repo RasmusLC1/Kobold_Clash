d7395552ccdd08b6295f5c258c4075f47cb7065a
RasmusLC1 <rasmus.lc3@gmail.com>
2025-07-28 16:19:26 +0200

    Update boss_room.py

--- scripts/entities/rooms/boss_room.py
+++ scripts/entities/rooms/boss_room.py
@@ -2,12 +2,22 @@ import random
 from scripts.engine.keys.keys import keys
 
 
-class Library():
-    def Spawn_Library(self, game):
-        libraries = game.tilemap.extract([(keys.room, keys.library)])
+class Boss_Room():
+    def Spawn_Boss_Room(self, game):
+        boss_rooms = game.tilemap.extract([(keys.room, keys.library)])
         decorations = {}
 
-        for library in libraries:
-            decorations.update(self.Spawn_Library_Decoration(game.tilemap, decorations, library))
+        if keys.boss_room not in decorations:
+            decorations[keys.boss_room] = []
+
+        for boss_room in boss_rooms:
+            decorations.update(self.Spawn_Boss_Room(decorations, boss_room))
         return decorations
+    
+    # More traps, rewards etc can be spawned later
+    def Spawn_Boss_Room(self, decorations, boss_room):
+        pos = boss_room[keys.pos]
+
+        decorations[keys.boss_room].append(pos)
+
   
