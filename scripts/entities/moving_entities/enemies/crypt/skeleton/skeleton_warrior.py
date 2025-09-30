7234c563991c6e9c78af0f733f59e86b0c08480a
RasmusLC1 <145757764+RasmusLC1@users.noreply.github.com>
2025-09-30 16:56:59 +0200

    Skeleton warrior sprite update

--- scripts/entities/moving_entities/enemies/crypt/skeleton/skeleton_warrior.py
+++ scripts/entities/moving_entities/enemies/crypt/skeleton/skeleton_warrior.py
@@ -8,7 +8,7 @@ import random
 
 class Skeleton_Warrior(Skeleton):
     def __init__(self, game, pos, health, strength, max_speed, agility, intelligence, stamina):
-        type = str(random.randint(1, 2))
+        type = str(random.randint(1, 3))
         super().__init__(game, pos, keys.skeleton_warrior + '_' + type, health, strength, max_speed, agility, intelligence, stamina, 0.8, 10)
         self.intent_manager.Set_Intent([keys.direct, keys.attack, keys.attack, keys.attack, keys.medium_range])
         self.Equip_Weapon(Claw(game, self.pos)) 
@@ -25,8 +25,7 @@ class Skeleton_Warrior(Skeleton):
         self.active_weapon.render = False
         del(weapon)
         return True
+    
     def Render_Weapons(self, surf, offset):
         pass
 
-    def Set_Max_Charge(self):
-        self.max_weapon_charge = 1.8 - self.active_weapon.speed / 10
